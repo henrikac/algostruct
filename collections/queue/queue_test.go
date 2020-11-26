e5d089674b651bd54d048bc15bfceae87aa4bba2
Henrik Christensen <sensen1695@hotmail.com>
2020-11-26 22:40:50 +0100

    removed import parentheses

--- collections/queue/queue_test.go
+++ collections/queue/queue_test.go
@@ -1,8 +1,6 @@
 package queue
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestNew(t *testing.T) {
 	q := New()
--- collections/stack/stack_test.go
+++ collections/stack/stack_test.go
@@ -1,8 +1,6 @@
 package stack
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestNew(t *testing.T) {
 	s := New()
